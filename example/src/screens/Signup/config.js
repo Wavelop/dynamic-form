401175e042e0b2c6069d6d300a23c0f2f54160bc
Matteo Granzotto <granzotto.matteo@gmail.com>
2020-08-10 13:02:19 +0200

    feat(#27): removed not necessary config

--- example/src/screens/Signup/config.js
+++ example/src/screens/Signup/config.js
@@ -230,23 +230,23 @@ export const form = ({ t, dynamics }) => {
         { value: "Journalist", name: t("Signup.form.occupation.Journalist") }
       ]
     },
-    {
-      name: "nickName",
-      label: t("Signup.form.nickName"),
-      helperText: t("Signup.form.nickName.helperText"),
-      tag: TextField,
-      type: "text",
-      attributes: {
-        multiline: true,
-        variant: 'standard'
-      },
-      validations: [
-        {
-          kind: "required",
-          message: t("Error.message.required")
-        }
-      ]
-    },
+    // {
+    //   name: "nickName",
+    //   label: t("Signup.form.nickName"),
+    //   helperText: t("Signup.form.nickName.helperText"),
+    //   tag: TextField,
+    //   type: "text",
+    //   attributes: {
+    //     multiline: true,
+    //     variant: 'standard'
+    //   },
+    //   validations: [
+    //     {
+    //       kind: "required",
+    //       message: t("Error.message.required")
+    //     }
+    //   ]
+    // },
     // {
     //   name: "interests",
     //   label: t("Signup.form.interests"),
