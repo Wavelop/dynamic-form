612a7676497f7d65355496f23e3eb154523b9b4d
Matteo Granzotto <granzotto.matteo@gmail.com>
2020-08-13 17:26:14 +0200

    feat(#30): update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 module.exports = {
-  stories: ["../src/**/*.stories.([tj]s|mdx)"],
+  stories: ["../packages/**/src/**/*.stories.([tj]s|mdx)"],
   addons: [
     
     // First box
-    "@storybook/preset-create-react-app",
     {
       name: '@storybook/addon-docs',
       options: {
@@ -16,7 +15,6 @@ module.exports = {
     
     // Second box
     "@storybook/addon-knobs/register",
-    "@storybook/addon-jest/register",
     "@storybook/addon-actions/register",
   ]
 };
