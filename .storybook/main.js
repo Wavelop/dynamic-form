23ec2b0119f7b6a8d9570d3d971ee29604d36030
Matteo Granzotto <granzotto.matteo@gmail.com>
2020-08-13 23:03:11 +0200

    fix: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,15 @@
 module.exports = {
-  stories: ["../packages/**/src/**/*.stories.@([tj]s|mdx)"],
   webpackFinal: async config => {
     config.performance = { hints: false };
     return config;
   },
-  addons: [
-    
-    // First box
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-      },
-    },
-    "@storybook/addon-viewport/register",
-    
-    // Second box
-    "@storybook/addon-knobs/register",
-    "@storybook/addon-actions/register",
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
   ]
-};
+}
